88835bbec41f829b3caba41df9caf85b2218c616
SuhasBharadwajK <suhas.five@gmail.com>
2014-07-12 23:55:11 +0530

    --

--- script.js
+++ script.js
@@ -1,41 +1,15 @@
 $(document).ready(function() {
-//    $("#top").mouseenter(function() {
-//        $(this).animate({
-//            width: '+=100em',
-//            height: '+=100em'
-//        });
-//    });
-//    $("#top").mouseleave(function() {
-//        $(this).animate({
-//            width: '-=100em',
-//            height: '-=100em'
-//        });
-//    });
-     $("#top").onClick(function() {
+    $("#top").mouseenter(function() {
         $(this).animate({
             width: '+=100em',
             height: '+=100em'
         });
     });
-//    $("#top").mouseleave(function() {
-//        $(this).animate({
-//            width: '-=100em',
-//            height: '-=100em'
-//        });
-    });
-     $("#right").mouseenter(function() {
-        $(this).animate({
-            width: '+=100em',
-            height: '+=100em'
-        });
-    });
-    $("#right").mouseleave(function() {
+    $("#top").mouseleave(function() {
         $(this).animate({
             width: '-=100em',
             height: '-=100em'
         });
     });
-
-
 });
 //http://css-tricks.com/almanac/properties/t/transition/
