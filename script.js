d868e64c65450089c02580046f9490b7fc55c76d
SuhasBharadwajK <suhas.five@gmail.com>
2014-07-13 11:46:17 +0530

    Final changes done from the laptop

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 $(document).ready(function() {
-    $("#top").mouseenter(function() {
-        $(this).animate({
-            width: '+=100em',
-            height: '+=100em'
-        });
+//    $("#top").mouseenter(function() {
+//        $(this).animate({
+//            width: '+=100em',
+//            height: '+=100em'
+//        });
+//    });
+//    $("#top").mouseleave(function() {
+//        $(this).animate({
+//            width: '-=100em',
+//            height: '-=100em'
+//        });
+//    });
+    $("#top").click(function() {
+        //$("#right").animate({ right : '-=500px'},3000, function() { $(this).fadeOut() {400, ease ,function() {$(this).hide()}}});
+        //$("#right").fadeOut(3000,"linear", function() { $(this).animate({right:'-=500px'},3000)});
+        //$("#right").hide().show("fade", {},  {duration:1200}).effect("bounce", { times:3 },  { duration:400}).dequeue();
+        //$("#right").fadeOut
     });
-    $("#top").mouseleave(function() {
-        $(this).animate({
-            width: '-=100em',
-            height: '-=100em'
-        });
+    $("#right").click(function() {
+        $(this).hide();
+    });
+    $("#left").click(function() {
+        $("#right").show();
     });
 });
 //http://css-tricks.com/almanac/properties/t/transition/
+//http://stackoverflow.com/questions/1251300/how-to-run-two-jquery-animations-simultaneously
+//http://stackoverflow.com/questions/10473572/execute-multiple-jquery-effects-on-the-same-element-in-parallel
