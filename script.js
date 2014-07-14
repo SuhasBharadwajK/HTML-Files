0abde7007f5651b3c75c1bc6500ef63d5bad4223
SuhasBharadwajK <suhas.five@gmail.com>
2014-07-14 13:05:02 +0530

    First phase of animations
    The first step in the page creation with three circles going away from
    each other and disappearing when clicked on one of them.

--- script.js
+++ script.js
@@ -16,19 +16,38 @@ $(document).ready(function() {
         //$("#right").fadeOut(3000,"linear", function() { $(this).animate({right:'-=500px'},3000)});
         //$("#right").hide().show("fade", {},  {duration:1200}).effect("bounce", { times:3 },  { duration:400}).dequeue();
         //$("#right").fadeOut
-        $("#right")
-            .animate( {opacity: 0.25, right : '-=150px'},1000)
+        $("#right").animate( {opacity: 0.25, left : '+=150px'},1000).fadeTo(1000,0, function() {$(this).hide()});
+            
             //.animate( {opacity: 0.25, right: '-=150px', /*height: "toggle"*/});
-            .fadeTo(1000,0, function() {$(this).hide()});
-        $("#left")
-            .animate( {opacity: 0.25, right : '-=150px', 1000)
+            
+        $("#left").animate( {opacity: 0.25, left : '-=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
+        $("#bottom").animate( {opacity: 0.25, top : '+=150px'}, 1000).queue(function() { $(this).fadeTo(1000,0,function() {$(this).hide()})}).dequeue();
+        $(this).animate({top: '+=150px'},2000);
+    });
+    $("#bottom").click(function() {
+       $("#right").animate( {opacity: 0.25, left : '+=150px'},1000).fadeTo(1000,0, function() {$(this).hide()});
+        $("#left").animate( {opacity: 0.25, left : '-=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
+        $("#top").animate( {opacity: 0.25, top : '-=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
+        $(this).animate({top: '-=180px'},2000);
     });
     $("#right").click(function() {
-        $(this).hide();
+       $("#bottom").animate( {opacity: 0.25, top : '+=150px'},1000).fadeTo(1000,0, function() {$(this).hide()});
+        $("#left").animate( {opacity: 0.25, left : '-=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
+        $("#top").animate( {opacity: 0.25, top : '-=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
+        $(this).animate({left: '-=320px'},2000);
     });
     $("#left").click(function() {
-        $("#right").show();
+       $("#bottom").animate( {opacity: 0.25, top : '+=150px'},1000).fadeTo(1000,0, function() {$(this).hide()});
+        $("#right").animate( {opacity: 0.25, left : '+=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
+        $("#top").animate( {opacity: 0.25, top : '-=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
+        $(this).animate({left: '+=320px'},2000);
     });
+//    $("#right").click(function() {
+//        $(this).hide();
+//    });
+//    $("#left").click(function() {
+//        $("#right").show();
+//    });
 });
 //http://css-tricks.com/almanac/properties/t/transition/
 //http://stackoverflow.com/questions/1251300/how-to-run-two-jquery-animations-simultaneously
