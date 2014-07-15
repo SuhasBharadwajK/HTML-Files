53dbf7849c3270cadccf1fa973b80f494b41d3f8
SuhasBharadwajK <suhas.five@gmail.com>
2014-07-15 15:11:47 +0530

    Second phase of animations
    All the four major divs working properly. Three move away and one goes
    into middle when clicked on it and the state is reset by animation,
    using a reset button.

--- script.js
+++ script.js
@@ -1,59 +1,58 @@
-$(document).ready(function() {
-//    $(".top").mouseenter(function() {
-//        $(this).animate({
-//            width: '+=100em',
-//            height: '+=100em'
-//        });
-//    });
-//    $(".top").mouseleave(function() {
-//        $(this).animate({
-//            width: '-=100em',
-//            height: '-=100em'
-//        });
-//    });
-    $(".top").click(function() {
-        //$(".right").animate({ right : '-=500px'},3000, function() { $(this).fadeOut() {400, ease ,function() {$(this).hide()}}});
-        //$(".right").fadeOut(3000,"linear", function() { $(this).animate({right:'-=500px'},3000)});
-        //$(".right").hide().show("fade", {},  {duration:1200}).effect("bounce", { times:3 },  { duration:400}).dequeue();
-        //$(".right").fadeOut
-        $(".right").animate( {opacity: 0.25, left : '+=150px'},1000).fadeTo(1000,0, function() {$(this).hide()});
-            
-            //.animate( {opacity: 0.25, right: '-=150px', /*height: "toggle"*/});
-            
-        $(".left").animate( {opacity: 0.25, left : '-=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
-        $(".bottom").animate( {opacity: 0.25, top : '+=150px'}, 1000).queue(function() { $(this).fadeTo(1000,0,function() {$(this).hide()})}).dequeue();
-        $(this).animate({top: '+=150px'},2000);
+var clickCallBacks = jQuery.Callbacks;
+var flag = 1;
+$(document).ready(function starts(){
+    $(".btop").click(function() {
+        $("#right").animate( {opacity: 0.25, left : '+=150px'},1000).fadeTo(1000,0, function() {$(this).hide()});
+        $("#left").animate( {opacity: 0.25, left : '-=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
+        $("#bottom").animate( {opacity: 0.25, top : '+=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
+        $("#top").animate({top: '+=150px'},2000, function() {$(".btop").remove()});
+        $(".buttoncontainer").append('<div class="st"></div>');
+        $(".st").click(function() {
+            $("#top").animate({ top: '-=150px'}, 2000);
+            $("#left").fadeTo(1000,1, function() {$(this).show()}).animate({left:'+=150px'},1000);
+            $("#right").fadeTo(1000,1, function() {$(this).show()}).animate({left:'-=150px'},1000, function() {$("#top").prepend('<div class="btop"></div>')});
+            $("#bottom").fadeTo(1000,1, function() {$(this).show()}).animate({top:'-=150px'},1000, function() {$(".st").remove();starts();});
+        });
     });
-    $(".bottom").click(function() {
-       $(".right").animate( {opacity: 0.25, left : '+=150px'},1000).fadeTo(1000,0, function() {$(this).hide()});
-        $(".left").animate( {opacity: 0.25, left : '-=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
-        $(".top").animate( {opacity: 0.25, top : '-=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
-        $(this).animate({top: '-=180px'},2000);
+    $(".bbottom").click(function() {
+       $("#right").animate( {opacity: 0.25, left : '+=150px'},1000).fadeTo(1000,0, function() {$(this).hide()});
+        $("#left").animate( {opacity: 0.25, left : '-=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
+        $("#top").animate( {opacity: 0.25, top : '-=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
+        $("#bottom").animate({top: '-=180px'},2000, function() {$(".bbottom").remove()});
+        $(".buttoncontainer").append('<div class="sb"></div>');
+        $(".sb").click(function() {
+            $("#bottom").animate({ top: '+=150px'}, 2000);
+            $("#left").fadeTo(1000,1, function() {$(this).show()}).animate({left:'+=150px'},1000);
+            $("#right").fadeTo(1000,1, function() {$(this).show()}).animate({left:'-=150px'},1000, function() {$("#bottom").prepend('<div class="bbottom"></div>')});
+            $("#top").fadeTo(1000,1, function() {$(this).show()}).animate({top:'+=150px'},1000, function() {$(".sb").remove();starts();});
+        });
     });
-    $(".right").click(function() {
-       $(".bottom").animate( {opacity: 0.25, top : '+=150px'},1000).fadeTo(1000,0, function() {$(this).hide()});
-        $(".left").animate( {opacity: 0.25, left : '-=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
-        $(".top").animate( {opacity: 0.25, top : '-=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
-        $(this).animate({left: '-=320px'},2000);
+    $(".bright").click(function() {
+       $("#bottom").animate( {opacity: 0.25, top : '+=150px'},1000).fadeTo(1000,0, function() {$(this).hide()});
+        $("#left").animate( {opacity: 0.25, left : '-=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
+        $("#top").animate( {opacity: 0.25, top : '-=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
+        $("#right").animate({left: '-=320px'}, 2000, function() {$(".bright").remove()});
+        $(".buttoncontainer").append('<div class="sr"></div>');
+        $(".sr").click(function() {
+            $("#right").animate({ left: '+=320px'}, 2000);
+            $("#left").fadeTo(1000,1, function() {$(this).show()}).animate({left:'+=150px'},1000);
+            $("#bottom").fadeTo(1000,1, function() {$(this).show()}).animate({top:'-=150px'},1000, function() {$("#right").prepend('<div class="bright"></div>')});
+            $("#top").fadeTo(1000,1, function() {$(this).show()}).animate({top:'+=150px'},1000, function() {$(".sr").remove();starts();});
+        });
     });
-    $(".left").click(function() {
-       $(".bottom").animate( {opacity: 0.25, top : '+=150px'},1000).fadeTo(1000,0, function() {$(this).hide()});
-        $(".right").animate( {opacity: 0.25, left : '+=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
-        $(".top").animate( {opacity: 0.25, top : '-=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
-        $(this).animate({left: '+=320px'},2000);
+    $(".bleft").click(function() {
+        $("#bottom").animate( {opacity: 0.25, top : '+=150px'},1000).fadeTo(1000,0, function() {$(this).hide()});
+        $("#right").animate( {opacity: 0.25, left : '+=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
+        $("#top").animate( {opacity: 0.25, top : '-=150px'}, 1000).fadeTo(1000,0,function() {$(this).hide()});
+        $("#left").animate({left: '+=320px'},2000, function(){$(".bleft").remove()});
+        $(".buttoncontainer").append('<div class="sl"></div>');
+        $(".sl").click(function() {
+            $("#left").animate({ left: '-=320px'}, 2000);
+            $("#right").fadeTo(1000,1, function() {$(this).show()}).animate({left:'-=150px'},1000);
+            $("#bottom").fadeTo(1000,1, function() {$(this).show()}).animate({top:'-=150px'},1000, function() {$("#left").prepend('<div class="bleft"></div>')});
+            $("#top").fadeTo(1000,1, function() {$(this).show()}).animate({top:'+=150px'},1000, function() {$(".sl").remove();starts();});
+        });
     });
-    $("#showall").click(function() {
-        $(".top").animate({ top: '-=150px'}, 2000);
-        $(".left").fadeTo(1000,1, function() {$(this).show()}).animate({left:'+=150px'},1000);
-        $(".right").show();
-        $(".bottom").show();
-    });
-//    $(".right").click(function() {
-//        $(this).hide();
-//    });
-//    $(".left").click(function() {
-//        $(".right").show();
-//    });
 });
 //http://css-tricks.com/almanac/properties/t/transition/
 //http://stackoverflow.com/questions/1251300/how-to-run-two-jquery-animations-simultaneously
